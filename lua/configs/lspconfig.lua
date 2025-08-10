d36381a266f9bacd56084d10b115ad7cd3c74e8b
Peyman khalili <p.zonouz@gmail.com>
2025-08-10 23:21:46 +0330

    latest

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,82 +5,81 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 local servers = {
-	"html",
-	"cssls",
-	"ts_ls",
-	"djlsp",
-	-- "eslint",
-	"gopls",
-	"golangci_lint_ls",
-	"templ",
-	"clangd",
-	"intelephense",
-	"marksman",
+  "html",
+  "cssls",
+  "ts_ls",
+  "djlsp",
+  "tailwindcss",
+  "gopls",
+  "golangci_lint_ls",
+  "templ",
+  "clangd",
+  "marksman",
 }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		on_init = on_init,
-		capabilities = capabilities,
-	})
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  })
 end
 
 -- C#
 lspconfig.omnisharp.setup({
-	settings = {
-		omnisharp = {
-			useModernNet = true, -- Optional: Use modern .NET SDK
-		},
-	},
-	cmd = { "/usr/bin/omnisharp" }, -- Replace with your OmniSharp executable path
-	filetypes = { "cs" },
-	root_dir = function(fname)
-		return lspconfig.util.root_pattern("*.sln", "*.csproj")(fname) or lspconfig.util.path.dirname(fname)
-	end,
-	on_attach = on_attach,
-	on_init = on_init,
-	capabilities = capabilities,
+  settings = {
+    omnisharp = {
+      useModernNet = true, -- Optional: Use modern .NET SDK
+    },
+  },
+  cmd = { "/usr/bin/omnisharp" }, -- Replace with your OmniSharp executable path
+  filetypes = { "cs" },
+  root_dir = function(fname)
+    return lspconfig.util.root_pattern("*.sln", "*.csproj")(fname) or lspconfig.util.path.dirname(fname)
+  end,
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
 })
 
 --emmet
 lspconfig.emmet_language_server.setup({
-	filetypes = {
-		"css",
-		"eruby",
-		"html",
-		"javascript",
-		"javascriptreact",
-		"less",
-		"sass",
-		"scss",
-		"pug",
-		"typescriptreact",
-	},
-	lspconfig.pyright.setup({
-		filetypes = { "python" },
-	}),
+  filetypes = {
+    "css",
+    "eruby",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "less",
+    "sass",
+    "scss",
+    "pug",
+    "typescriptreact",
+  },
+  lspconfig.pyright.setup({
+    filetypes = { "python" },
+  }),
 
-	--emmet
-	init_options = {
-		---@type table<string, string>
-		includeLanguages = {},
-		--- @type string[]
-		excludeLanguages = {},
-		--- @type string[]
-		extensionsPath = {},
-		--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-		preferences = {},
-		--- @type boolean Defaults to `true`
-		showAbbreviationSuggestions = true,
-		--- @type "always" | "never" Defaults to `"always"`
-		showExpandedAbbreviation = "always",
-		--- @type boolean Defaults to `false`
-		showSuggestionsAsSnippets = false,
-		--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-		syntaxProfiles = {},
-		--- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-		variables = {},
-	},
+  --emmet
+  init_options = {
+    ---@type table<string, string>
+    includeLanguages = {},
+    --- @type string[]
+    excludeLanguages = {},
+    --- @type string[]
+    extensionsPath = {},
+    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+    preferences = {},
+    --- @type boolean Defaults to `true`
+    showAbbreviationSuggestions = true,
+    --- @type "always" | "never" Defaults to `"always"`
+    showExpandedAbbreviation = "always",
+    --- @type boolean Defaults to `false`
+    showSuggestionsAsSnippets = false,
+    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+    syntaxProfiles = {},
+    --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+    variables = {},
+  },
 })
